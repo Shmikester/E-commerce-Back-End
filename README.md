e506e1ea8743e1f2c65c9447af2b69dfca1d7967
Heff <shmikester@gmail.com>
2021-12-11 18:41:34 -0700

    full push

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# E-commerce-Back-End
+# E-Commerce-Backend
+  
+  ## Description
+  this app is the backend setup for a E-Commerce site. Uses JS, JSON, and SQL.
+
+  ## Credits
+  Michael 'Heff' Heffron
+  ## Questions
+  If you have any questions please contact me at: [Shmikester@gmail.com](Shmikester@gmail.com)
+  This is my GitHub: [Shmikester](https://github.com/Shmikester)
+  
+  
---
+++ routes/api/tag-routes.js
@@ -0,0 +1,100 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', (req, res) =>
+{
+  // find all tags
+  // be sure to include its associated Product data
+  Tag.findAll({
+    include: Product
+  }).then(tagData =>
+  {
+    res.json(tagData);
+  }).catch((err) =>
+  {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+router.get('/:id', (req, res) =>
+{
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Product]
+  }).then(oneTagData =>
+  {
+    if (!oneTagData)
+    {
+      return res.status(404).json({ message: `No tag with ID: ${req.params.id}` });
+    }
+    res.json(oneTagData);
+  }).catch((err) =>
+  {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+router.post('/', (req, res) =>
+{
+  // create a new tag
+  Tag.create(req.body).then(tagAdd =>
+  {
+    res.json(tagAdd);
+  }).catch((err) =>
+  {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+router.put('/:id', (req, res) =>
+{
+  // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  }).then(tagUpdate =>
+  {
+    if (!tagUpdate)
+    {
+      return res.status(404).json({ message: `No tag with ID: ${req.params.id}` });
+    }
+    res.json(tagUpdate);
+  }).catch((err) =>
+  {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+router.delete('/:id', (req, res) =>
+{
+  // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(tagDelete =>
+  {
+    if (!tagDelete)
+    {
+      return res.status(404).json({ message: `No tag with ID: ${req.params.id}` });
+    }
+    res.json(tagDelete);
+  }).catch((err) =>
+  {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+module.exports = router;
